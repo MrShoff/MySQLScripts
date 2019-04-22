9a2f5243a243c8b4c5b6f34e7a0fa45994bf67c6
MrShoff <34757410+MrShoff@users.noreply.github.com>
2019-04-22 09:01:45 -0400

    Delete postcount_frequent_queries.sql

--- MySQL Scripts/postcount_frequent_queries.sql
+++
@@ -1,9 +0,0 @@
-USE postcount;
-
-SELECT		HTML_UnEncode(message_text), p.thread_url
-FROM		post p
-JOIN		user u
-	ON p.user_id = u.user_id
-WHERE		u.username = 'TheKingOfTexas'
-			##AND p.message_text like '%SAMANTHA%';
-            AND p.message_text like '%hell%';
